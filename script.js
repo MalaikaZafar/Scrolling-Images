9bc2165a8a21e5d5f4a68e56ea587dc13a8b8cb2
MalaikaZafar <malaikazafarpk@gmail.com>
2024-07-23 18:20:19 +0500

    Project Done

---
+++ script.js
@@ -0,0 +1,19 @@
+const card = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", animateCards);
+
+function animateCards() {
+  const checkBottom = (window.innerHeight / 4) * (125 / 25);
+
+  card.forEach((singleCard) => {
+    const cardTop = singleCard.getBoundingClientRect().top;
+
+    let toSlideIn = cardTop < checkBottom;
+
+    if (toSlideIn) {
+      singleCard.classList.add("slidingIn");
+    } else {
+      singleCard.classList.remove("slidingIn");
+    }
+  });
+}
